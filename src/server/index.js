e13f1801a501cd1de2c0c6353cd245efb2ac7aa4
Sivakumar Rajendran <emails2shiva@gmail.com>
2020-04-13 15:06:32 +0530

    Added initial Webpack configurations

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001!')
 })
 
 app.get('/test', function (req, res) {
